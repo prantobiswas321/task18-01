c9190cde4f991fbf7ddbe6fa2fb02cf4c200baef
prantobiswas321 <rahulbd321@gmail.com>
2022-01-17 23:08:04 +0600

    components and nav done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route>Not Found</Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-color">
+            <div className="container">
+                <h2 className="fw-bold header-text-color"><span className='text-warning'>C</span>ontact<span className='text-danger'>L</span>ist</h2>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="ms-auto fs-4 fw-bold navbar-nav">
+                        <ul className="navbar-nav mb-2 mb-lg-0">
+                            <li className="nav-item navLinks">
+                                <NavLink style={({ isActive }) => {
+                                    return {
+                                        display: "block",
+                                        // margin: "1rem 0",
+                                        color: isActive ? "red" : "yellow"
+                                    };
+                                }}
+                                    className="me-3 text-decoration-none" to="/home" >Home</NavLink>
+                            </li>
+
+                            <li className="nav-item navLinks">
+                                <NavLink className="me-3 text-decoration-none" to="/login" style={({ isActive }) => {
+                                    return {
+                                        display: "block",
+                                        // margin: "1rem 0",
+                                        color: isActive ? "red" : "yellow"
+                                    };
+                                }}>Login</NavLink>
+                            </li>
+
+                            <li className="nav-item navLinks">
+                                <NavLink className="me-3 text-decoration-none" to="/Register" style={({ isActive }) => {
+                                    return {
+                                        display: "block",
+                                        // margin: "1rem 0",
+                                        color: isActive ? "red" : "yellow"
+                                    };
+                                }} >Register</NavLink>
+                            </li>
+
+                            {
+                                // user.email &&
+                                // <li className="nav-item">
+                                //     <NavLink style={{ color: 'yellow' }} className="me-3 text-decoration-none navLinks" to="/dashboard" activeClassName="selected"
+                                //         activeStyle={{ color: '#00FFFF' }} >Dashboard</NavLink>
+                                // </li>
+                            }
+
+                            {/* {!user.email &&
+                                <li className="nav-item navLinks">
+                                    <NavLink style={{ color: 'yellow' }} className="me-3 text-decoration-none" to="/register" activeClassName="selected"
+                                        activeStyle={{ color: '#00FFFF' }}>Register</NavLink>
+                                </li> */}
+                            {/* } */}
+
+                            {
+                                // user?.email ?
+                                //     <button className="btn btn-success" onClick={logOut} > <span className="fw-bold text-info">Log Out</span> {user.displayName}</button>
+                                //     :
+                                //     <li className="nav-item">
+                                //         <NavLink style={{ color: 'yellow' }} className="me-3 text-decoration-none navLinks" to="/login" activeClassName="selected"
+                                //             activeStyle={{ color: '#00FFFF' }} >Login</NavLink>
+                                //     </li>
+                            }
+                        </ul>
+
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
